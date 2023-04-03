521d4b5a57cea62ad00e9f940e305bcd3a9f25a8
Sharon Ejikeme <ejikemesharon@gmail.com>
2023-04-03 04:11:07 +0100

    Updated Security classes Connected API to my Auth0 tenant

--- src/main/java/com/gmail/ejikemesharon/University/Management/System/security/SecurityConfig.java
+++ src/main/java/com/gmail/ejikemesharon/University/Management/System/security/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.gmail.ejikemesharon.University.Management.System.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -16,6 +19,10 @@ import java.util.List;
 
 @EnableGlobalMethodSecurity
 public class SecurityConfig {
+    @Value("")
+    private String audience;
+    @Value("")
+    private String issuer;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -31,7 +38,7 @@ public class SecurityConfig {
                 .oauth2ResourceServer()
                 .jwt()
                 .decoder(decoder())
-                .jwtAuthenticationConverter();
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
 
         return httpSecurity.build();
 
@@ -52,6 +59,22 @@ public class SecurityConfig {
     }
 
     private JwtDecoder decoder() {
-        OAuth2TokenValidator<Jwt> withAudience = new AudienceValidator
+        OAuth2TokenValidator<Jwt> withAudience = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withAudience, withIssuer);
+
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
+        jwtDecoder.setJwtValidator(validator);
+        return jwtDecoder;
+    }
+
+     private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("permissions");
+        converter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return authenticationConverter;
     }
 }
